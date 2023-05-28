e7e9d0cdd55fb9229e145ffb42173c61b48a2cfc
Elena_Remennaya <elena.oleksenko12@gmail.com>
2023-05-28 16:46:58 +0300

    add changes

--- src/index.js
+++ src/index.js
@@ -7,12 +7,9 @@ const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
 
-const PORT = 8080;
 require('dotenv').config();
 
-// mongoose.connect('mongodb://localhost:27017/myapp');
-mongoose.connect('mongodb+srv://OlenaOleksenko:5227337109531N@olenaoleksenkomongodbcl.jwck9ba.mongodb.net/users?retryWrites=true&w=majority');
-// mongoose.connect(process.env.DB_HOST);
+mongoose.connect(process.env.DB_HOST);
 
 const { authRouter } = require('./routers/authRouter');
 const { userRouter } = require('./routers/userRouter');
@@ -33,8 +30,8 @@ app.use('/api/loads', authMiddleware, loadRouter);
 
 const start = async () => {
   try {
-    app.listen(PORT, () => {
-      console.log(`Server started on ${PORT}`);
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server started on ${process.env.PORT}`);
     });
   } catch (err) {
     console.error(`Error on server startup: ${err.message}`);
