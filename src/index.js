04963faeb76cb386a42cd48e47f93a93551039c2
Elena_Remennaya <elena.oleksenko12@gmail.com>
2023-05-31 10:04:24 +0300

    update index file

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(morgan('tiny'));
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use('/public/photos', express.static('public/photos'));
+app.use('/public/photos', express.static(path.resolve(__dirname, 'public', 'photos')));
 app.use('/api/auth', authRouter);
 app.use('/api/users/me', authMiddleware, userRouter);
 app.use('/api/trucks', authMiddleware, truckRouter);
