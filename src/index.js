d8fa662edc9ea99046d2d80d58a3cf9e8905f8ad
Elena_Remennaya <elena.oleksenko12@gmail.com>
2023-05-31 14:19:48 +0300

    add changes

--- src/index.js
+++ src/index.js
@@ -22,10 +22,10 @@ app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../public')));
 app.get('/reset-password/:resetToken/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
+app.use(express.static(path.join(__dirname, '../public')));
 app.use('/public/photos', express.static(path.resolve(__dirname, 'public', 'photos')));
 app.use('/api/auth', authRouter);
 app.use('/api/users/me', authMiddleware, userRouter);
